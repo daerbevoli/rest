43f145ed850c6538aee2793a40432d98ca529450
Sameer <101348238+daerbevoli@users.noreply.github.com>
2024-03-17 13:57:02 +0100

    final commit

--- src/main/java/be/uantwerpen/fti/ei/rest/BankService.java
+++ src/main/java/be/uantwerpen/fti/ei/rest/BankService.java
@@ -7,21 +7,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.*;
 
 /**
- * The BankService class with which the client can interact using REST.
- * It uses a concurrentHashMap to store key value pairs where the key is the hashnumber of the account
- * and the value is the account object.
- * it has GET methods to het the object properties, PUT methods to deposit and withdraw money
- * and a POST method to create an account.
+ * The BankService class with which the client can interact.
+ * It uses a concurrentHashMap to store key value pairs where the key is the hashnumber of the account and the value is the account object.
+ * It has GET methods to get the object properties, PUT methods to deposit and withdraw money and a POST method to create an account.
  * The creation of the account takes as request body as JSON file with key value pairs and
  * returns a unique hashcode associated with the account.
  * With this hashcode, the client can take actions on the associated account.
 
-* Below I will explain all the annotations.
-* @RestController indicates the controller in the application. It is reponsible for handling HTTP resquests. 
-* @RequestMapping is used to map incoming requests to specifc handlers. This applicationn can be reached at the handle '/bank'
-* @PostMapping, @GetMapping etc. are specific handles methods corresponding to CRUD operations.
-* @PathVariable is a handle method to bind a parameter to a URI template variable. 
-* 
+ * Below I will explain all the annotations.
+ * @RestController indicates the controller in the application. It is reponsible for handling HTTP requests. 
+ * @RequestMapping is used to map incoming requests to specifc handlers. This application can be reached at the handle '/bank'
+ * @PostMapping, @GetMapping etc. are specific handles methods corresponding to CRUD operations.
+ * @PathVariable is a handle method to bind a parameter to a URI template variable. 
+ * 
 */
 
 
@@ -35,12 +33,17 @@ public class BankService {
     // The use here is justified because the assigment demanded that two clients could use a joint account concurrently.
     private final ConcurrentHashMap<Integer, BankAccount> map = new ConcurrentHashMap<>(1);
 
-    // The putIfAbsent() method is used to prevent account creation with the same account number.
+    // Create bank account and add it to the map. 
     @PostMapping("/create")
     public ResponseEntity<String> createAccount(@RequestBody BankAccount account){
-        map.putIfAbsent(account.getHash(), new BankAccount(account.getName(), account.getBalance(), account.getAccountNumber()));
-        return ResponseEntity.ok("Account created \nThis is your hashcode: " + account.getHash());
+        if (map.containsKey(account.getHash())) {
+            return ResponseEntity.badRequest().body("Account with account number " + account.getAccountNumber() + " already exists");
+        } else {
+            map.putIfAbsent(account.getHash(), new BankAccount(account.getName(), account.getBalance(), account.getAccountNumber()));
+            return ResponseEntity.ok("Account created \nThis is your hashcode: " + account.getHash());
+        }
     }
+
 
     // Get method to test remote nodes.
     @GetMapping("/get")
